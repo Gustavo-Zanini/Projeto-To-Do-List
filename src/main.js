78f59995430f5b027df10fa3a8ffef770a0892c1
Gustavo-Zanini <gugazanini1@gmail.com>
2025-09-26 14:30:21 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { inicializarUI, atualizarTabela } from './ui.js';
 
-// Inicializar a aplicação quando o DOM estiver carregado
 document.addEventListener('DOMContentLoaded', () => {
     inicializarUI();
 });
 
-// Exportar para uso no console (debug)
 window.app = {
     atualizarTabela
 };
